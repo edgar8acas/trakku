8c74ee66a6d2cf7aa49c2af45d2a15ee5ce81305
Edgar Ochoa <eochoa3@ucol.mx>
2020-11-18 01:13:31 -0600

    add request wrapper

---
+++ packages/web/src/utilities/request.ts
@@ -0,0 +1,32 @@
+function request(endpoint: string, options: any) {
+  const headers = { "Content-Type": "application/json" };
+
+  const config: RequestInit = {
+    method: "GET",
+    headers: {
+      ...headers,
+      ...options.headers,
+    },
+    ...options,
+  };
+
+  if (options.body) {
+    config.body = JSON.stringify(options.body);
+  }
+
+  return window
+    .fetch(
+      `${process.env.REACT_APP_API_URL || "http://localhost:5000"}/${endpoint}`,
+      config
+    )
+    .then(async (response) => {
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    });
+}
+
+export default request;
