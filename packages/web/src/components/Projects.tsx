261873fae6b89c8567c105fd405c986910ebcbbe
Edgar Ochoa <eochoa3@ucol.mx>
2020-11-29 14:44:23 -0600

    add projects view

---
+++ packages/web/src/components/Projects.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Projects() {
+  return (
+    <div className="Projects">
+      <button className="button">Create project</button>
+    </div>
+  );
+}
+
+export default Projects;
--- packages/web/src/layouts/Dashboard.tsx
+++ packages/web/src/layouts/Dashboard.tsx
@@ -1,20 +1,39 @@
 import React from "react";
 import { useDispatch } from "react-redux";
+import { NavLink, Route, Switch, useRouteMatch } from "react-router-dom";
+import Projects from "../components/Projects";
 import { logout } from "../slices/auth";
 
 function Dashboard() {
   const dispatch = useDispatch();
+  const { path, url } = useRouteMatch();
+
   return (
     <div className="Dashboard">
       <div className="Sidebar">
         <h1>Logo</h1>
         <button onClick={() => dispatch(logout())}>Logout</button>
+        <nav>
+          <ul>
+            <li>
+              <NavLink to={`${url}/projects`}>Projects</NavLink>
+            </li>
+          </ul>
+        </nav>
       </div>
       <section className="Main">
         <header className="Main-header">
           <h2>Dashboard</h2>
-          <a href="#">Where</a> / <a href="#">am</a> / <a href="#">I</a> /
+          <a href="#a">Where</a> / <a href="#b">am</a> / <a href="#c">I</a> /
         </header>
+        <section className="Main-container">
+          <Switch>
+            <Route exact path={path}>
+              Dashboard
+            </Route>
+            <Route path={`${path}/projects`} component={Projects}></Route>
+          </Switch>
+        </section>
       </section>
     </div>
   );
