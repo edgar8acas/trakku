281932c3ba29403e44d697943d54d4811646dbcd
Edgar Ochoa <eochoa3@ucol.mx>
2020-12-06 22:45:06 -0600

    connect to db on production

--- packages/server/src/helpers/createOrmConnection.ts
+++ packages/server/src/helpers/createOrmConnection.ts
@@ -1,22 +1,18 @@
 import { Project } from "../entity/Project";
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 import { Issue } from "../entity/Issue";
 import { User } from "../entity/User";
 import { UserToProject } from "../entity/UserToProject";
 
 export const createOrmConnection = async () => {
-  return createConnection({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "postgres",
-    database: "we_track_server",
-    entities: [User, Issue, Project, UserToProject],
-    synchronize: true,
-  })
-    .then(async (connection) => {
-      console.log("Conected to database!");
-    })
-    .catch((error) => console.log(error));
+  const options = await getConnectionOptions(process.env.NODE_ENV);
+
+  return process.env.NODE_ENV === "production"
+    ? createConnection({
+        ...options,
+        url: process.env.DATABASE_URL,
+        entities: [User, Issue, Project, UserToProject],
+        name: "default",
+      } as any)
+    : createConnection({ ...options, name: "default" });
 };
