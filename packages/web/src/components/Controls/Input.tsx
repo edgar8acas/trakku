e33ac614c768f80adb20533a08a6cdb8a030d388
Edgar Ochoa <eochoa3@ucol.mx>
2020-12-08 22:45:34 -0600

    create input component

---
+++ packages/web/src/components/Controls/Input.tsx
@@ -0,0 +1,23 @@
+import { FieldProps } from "formik";
+import * as React from "react";
+
+const Input: React.FC<FieldProps<any> & { label: string }> = ({
+  field,
+  form: { touched, errors },
+  label,
+  ...props
+}) => {
+  return (
+    <div className="control">
+      <label>{label}</label>
+      <input {...field} {...props} />
+      {touched[field.name] && errors[field.name] ? (
+        <div className="error" role="alert">
+          {errors[field.name]}
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Input;
