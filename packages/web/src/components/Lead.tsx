2bbfca537f03b9b8f01d630ee1ddcfa3d3fabade
Edgar Ochoa <eochoa3@ucol.mx>
2020-12-29 16:04:30 -0600

    improve home page

--- packages/web/src/components/Lead.tsx
+++ packages/web/src/components/Lead.tsx
@@ -6,9 +6,11 @@ function Lead() {
       <div className="Lead__wrapper central-wrapper">
         <div className="Lead__heading">
           <h1>A simple issue tracker</h1>
-          <h2>Keep track of the important things</h2>
+          <h2>
+            Keep track of the <span className="important-word">important</span>{" "}
+            things
+          </h2>
         </div>
-        <div className="Lead__image"></div>
       </div>
     </div>
   );
--- packages/web/src/layouts/Home.tsx
+++ packages/web/src/layouts/Home.tsx
@@ -13,6 +13,17 @@ function Home() {
       <section className="Home__lead">
         <Lead />
       </section>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 1440 320"
+        className="Home-curved-shape"
+      >
+        <path
+          fill="#222222"
+          fill-opacity="1"
+          d="M0,128L120,138.7C240,149,480,171,720,149.3C960,128,1200,64,1320,32L1440,0L1440,320L1320,320C1200,320,960,320,720,320C480,320,240,320,120,320L0,320Z"
+        ></path>
+      </svg>
     </section>
   );
 }
